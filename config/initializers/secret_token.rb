688454b3f44cefeaa1f83941ae45d4a96d7663e8
Daniel Schmidt <dschmidt@weluse.de>
2013-02-25 16:45:12 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComparisonPortalApiPrototype::Application.config.secret_token = '2d89cf7d5379889e596a3d4c187dddb503f31efe7b1c0cde875a713f1899c97e533be96e24593a0587d2177ed2921f8a67e3cc7c9498a4c1716e3879f873669b'
